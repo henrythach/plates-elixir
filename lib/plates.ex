7c38297101daba9835c8ce2b55a22450d10273f3
Henry Thach <thachhao@gmail.com>
2018-12-15 13:45:06 -0500

    Add functions and tests

--- lib/plates.ex
+++ lib/plates.ex
@@ -9,17 +9,20 @@ defmodule Plates do
     [45, 35, 25, 10, 5, 2.5]
   end
 
-  def max_plate(weight) when weight <= 45, do: 0
-  def max_plate(weight) when weight >= 135, do: hd(available_plates())
-  def max_plate(weight) do
-    available_plates()
-      |> Stream.filter(&(&1 * 2 <= weight - barbell_weight()))
-      |> Enum.max
+  def plates(weight) when weight <= 45, do: []
+  def plates(weight) when rem(weight, 5) != 0, do: raise "not divisible by 5"
+  def plates(weight) do
+    plates_on_one_side((weight - 45) / 2)
   end
 
-  def plates(weight) when weight <= 45, do: []
+  def plates_on_one_side(weight) when weight <= 0, do: []
+  def plates_on_one_side(weight) do
+    [max_plate(weight)] ++ plates_on_one_side(weight - max_plate(weight))
+  end
 
-  def plates(weight) do
-    [(weight - 45) / 2] ++ plates(weight - 45)
+  def max_plate(weight) when weight >= 45, do: 45
+  def max_plate(weight) do
+    available_plates()
+      |> Enum.max_by(&(&1 <= weight))
   end
 end
--- test/plates_test.exs
+++ test/plates_test.exs
@@ -6,26 +6,33 @@ defmodule PlatesTest do
     assert Plates.available_plates() == [45, 35, 25, 10, 5, 2.5]
   end
 
-  test "barbell_weight" do
-    assert Plates.barbell_weight() == 45
-  end
+  test "barbell_weight", do: assert Plates.barbell_weight() == 45
 
   describe "max_plate" do
-    test "45 returns 0" do
-      assert Plates.max_plate(45) == 0
-    end
+    test "2.5", do: assert Plates.max_plate(2.5) == 2.5
+    test "5", do: assert Plates.max_plate(5) == 5
+    test "7.5", do: assert Plates.max_plate(7.5) == 5
+    test "10", do: assert Plates.max_plate(10) == 10
+    test "15", do: assert Plates.max_plate(15) == 10
+    test "20", do: assert Plates.max_plate(20) == 10
+    test "25", do: assert Plates.max_plate(25) == 25
+    test "35", do: assert Plates.max_plate(35) == 35
+    test "40", do: assert Plates.max_plate(40) == 35
+    test "45", do: assert Plates.max_plate(45) == 45
+    test "50", do: assert Plates.max_plate(50) == 45
+  end
 
-    test "50 returns 2.5" do
-      assert Plates.max_plate(50) == 2.5
-    end
-
-    test "135 returns 45" do
-      assert Plates.max_plate(135) == 45
-    end
-
-    test "140 returns 45" do
-      assert Plates.max_plate(140) == 45
-    end
+  describe "plates_on_one_side" do
+    test "2.5", do: assert Plates.plates_on_one_side(2.5) == [2.5]
+    test "5", do: assert Plates.plates_on_one_side(5) == [5]
+    test "10", do: assert Plates.plates_on_one_side(10) == [10]
+    test "15", do: assert Plates.plates_on_one_side(15) == [10, 5]
+    test "20", do: assert Plates.plates_on_one_side(20) == [10, 10]
+    test "30", do: assert Plates.plates_on_one_side(30) == [25, 5]
+    test "32.5", do: assert Plates.plates_on_one_side(32.5) == [25, 5, 2.5]
+    test "35", do: assert Plates.plates_on_one_side(35) == [35]
+    test "42.5", do: assert Plates.plates_on_one_side(42.5) == [35, 5, 2.5]
+    test "45", do: assert Plates.plates_on_one_side(45) == [45]
   end
 
   describe "plates" do
@@ -36,15 +43,12 @@ defmodule PlatesTest do
     test "under 45" do
       assert Plates.plates(40) == []
       assert Plates.plates(0) == []
-      assert Plates.plates(-1) == []
     end
 
-    test "65" do
-      assert Plates.plates(65) == [10]
+    test "not divisible by 5" do
+      assert_raise RuntimeError, "not divisible by 5", fn ->
+        Plates.plates(47)
+      end
     end
-
-    # test "70" do
-    #   assert Plates.plates(70) == [10, 2.5]
-    # end
   end
 end
